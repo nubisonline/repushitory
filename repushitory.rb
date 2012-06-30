d55ace00f3d7d079d33b1615bbf9c95a4254a395
Tim van Dalen <Tim@timvdalen.nl>
2012-06-30 23:34:44 +0200

    Added config files and git cloning

--- repushitory.rb
+++ repushitory.rb
@@ -1,6 +1,42 @@
 #!/usr/bin/ruby
 require 'socket'
 require 'json'
+require 'fileutils'
+require 'rubygems'
+require 'git'
+
+
+#Set up folders and configuration
+config = nil
+servers = nil
+
+progdir = File.join(Dir.home, ".repushitory")
+
+if(File.exists?(progdir))
+	Dir.chdir(progdir)
+	begin
+		confdata = File.read("config.json")
+		config = JSON.parse(confdata)
+	rescue Exception => e
+	end
+	begin
+		serverdata = File.read("servers.json")
+		servers = JSON.parse(serverdata)
+	rescue Exception => e
+	end
+else
+	Dir.mkdir(progdir, 0700)
+	Dir.chdir(progdir)
+end
+
+Dir.mkdir("repos", 0700) unless File.exists?("repos")
+
+if(config.nil? || servers.nil?)
+	puts "config.json or servers.json doesn't exists or isn't in the right (JSON) format. The files should be in " + progdir
+	Process.exit(1)
+end
+
+#Set up listening server
 webserver = TCPServer.new('127.0.0.1', 3210)
 while (session = webserver.accept)
 	contentlength = 0
@@ -16,9 +52,33 @@ while (session = webserver.accept)
 	content = session.read(contentlength)
 	session.print "HTTP/1.1 200/OK\nContent-type:text/html\n\n"
 	begin
+		#Parse payload
 		push = JSON.parse(content)
 		ref = push["ref"]
-		name = push["repository"]["name"]
+		repo = push["repository"]["name"]
+		owner = push["repository"]["owner"]["name"]
+		branch = ref.split("/").last
+
+		Dir.chdir("repos")
+
+		#Check config files for match
+		config["repositories"].each do |repository|
+			if(repository["owner"] == owner && repository["repo"] == repo)
+				#Repo is in the config file, take action
+				connection = "git@github.com:" + owner + "/" + repo + ".git"
+				repository = Git.clone(connection, repo)
+				repository.checkout(repository.branch(branch))
+
+				Dir.chdir(repo)
+
+				#Upload files to the deployment server
+
+				Dir.chdir("..")
+				FileUtils.rm_rf(repo)
+			end
+		end
+
+		Dir.chdir("..")
 	end
 	session.close
 end
